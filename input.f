ae9796f0cd21f05e7ea1c9c0e94dca9e6ffb660a
Frank <francescopaolo.castiglione@community.unipa.it>
2020-04-04 15:21:45 +0200

    Improved GPFSEL

--- input.f
+++ input.f
@@ -3,44 +3,6 @@ VARIABLE SIZE
 8 CONSTANT SIZE_REQUESTED
 
 
-
-: FALLING_EDGE_DETECT_SET_9				( -- )
-	GPFEN0 
-	9 1 MASK_REGISTER 
-	200 OR GPFEN0 ! ;
-	
-: FALLING_EDGE_DETECT_SET_10				( -- )
-	GPFEN0 
-	10 1 MASK_REGISTER 
-	400 OR GPFEN0 ! ;
-
-: SETUP_9 						( -- )		
-	SET_IN_9					\ We set the GPIOs as input and then we set the internal pull DOWN
-	GPPUDCLK0 9 1 MASK_REGISTER			\ This is for GPIO 9 
-	DOWN SET_PUD ;
-
-: SETUP_10						( -- )
-	SET_IN_10					\ We set the GPIOs as input and then we set the internal pull DOWN
-	GPPUDCLK0 10 1 MASK_REGISTER			\ This is for GPIO 10
-	DOWN SET_PUD ;
-
-: SETUP_BSC						( -- )
-	SET_ALT0_2					\ BSC1 is on GPIO pins 2(SDA) and 3(SCL) , so we set them to ALT0
-	SET_ALT0_3
-	SET_I2CEN ;					\ We enable the BSC controller
-	
-: FALLING_EDGE_DETECT_SET				( -- )
-	FALLING_EDGE_DETECT_SET_9
-	FALLING_EDGE_DETECT_SET_10 ;
-	
-: SETUP
-	SETUP_9
-	SETUP_10
-	SETUP_BSC
-	FALLING_EDGE_DETECT_SET 
-	0 CURRENT_VALUE !				
-	0 SIZE ! ;	
-
 : CLEAR_PIN
 	600 GPEDS0 ! ; 					\ We don't need to use a mask hence increase efficiency
 	
